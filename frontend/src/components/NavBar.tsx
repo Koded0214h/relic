1e1239cea69005e5b4f608db2eb9e4fef0d68356
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2026-08-31 02:03:22 +0100

    feat: made site responsive and worked on some more components

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,6 +1,15 @@
+import {useState} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+
 import './NavBar.css';
 
 function NavBar() {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <div className="navbar">
@@ -8,15 +17,21 @@ function NavBar() {
                 <h1>Relic.</h1>
             </div>
 
-            <ul className="nav-links">
-                <li><a href="#">Product</a></li>
-                <li><a href="#">How it works</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
+            <button className="hamburger-menu" onClick={toggle}>
+                {isOpen ? <FaTimes size={24} className='menu' /> : <FaBars size={24} className='menu' />}
+            </button>
 
-            <ul className='get-started'>
-                <li><a href="#">Join the waitlist</a></li>
-            </ul>
+            <div className={`nav-right ${isOpen ? 'show': ''}`}>
+                <ul className="nav-links">
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">How it works</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+
+                <ul className='get-started'>
+                    <li><a href="#">Join the waitlist</a></li>
+                </ul>
+            </div>
         </div>
     )
 }
